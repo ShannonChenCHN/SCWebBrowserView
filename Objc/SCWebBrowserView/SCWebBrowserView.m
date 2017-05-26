80b875514d6e191ab6e274b9e5284621098c2c43
ShannonChenCHN <273879688@qq.com>
2017-05-26 15:45:36 +0800

    Change class name: SCWebViewWrapperView -> SCWebBrowserView

---
+++ Objc/SCWebBrowserView/SCWebBrowserView.m
@@ -0,0 +1,31 @@
+//
+//  SCWebViewWrapperView.m
+//  SCWebViewWrapperView
+//
+//  Created by ShannonChen on 2017/5/24.
+//  Copyright © 2017年 ShannonChen. All rights reserved.
+//
+
+#import "SCWebViewWrapperView.h"
+
+@interface SCWebViewWrapperView ()
+
+@end
+
+@implementation SCWebViewWrapperView
+
+- (instancetype)initWithFrame:(CGRect)frame {
+    return [self initWithFrame:frame configuration:nil];
+}
+
+
+- (instancetype)initWithFrame:(CGRect)frame configuration:(WKWebViewConfiguration *)configuration {
+    if (self = [super initWithFrame:frame]) {
+        
+        
+    }
+    
+    return self;
+}
+
+@end
